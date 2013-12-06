1848f8df81057ed050e8ed47f79cbbb7e1856533
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-12-06 20:25:22 +0530

    Add sample UIs and routes

--- src/Panda86/MasterBundle/Controller/DashboardController.php
+++ src/Panda86/MasterBundle/Controller/DashboardController.php
@@ -8,7 +8,37 @@ class DashboardController extends Controller
 {
     public function indexAction()
     {
-        $name = 'World';
-        return $this->render('Panda86MasterBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('login-master.html.twig');
     }
+
+    public function dashboardAction()
+    {
+        return $this->render('Panda86MasterBundle:dashboard:index.html.twig');
+    }
+
+    public function clientAction()
+    {
+        return $this->render('Panda86MasterBundle:client:index.html.twig');
+    }
+
+    public function reportsAction()
+    {
+        return $this->render('Panda86MasterBundle:reports:index.html.twig');
+    }
+
+    public function annualAction()
+    {
+        return $this->render('Panda86MasterBundle:reports:annual.html.twig');
+    }
+
+    public function quarterAction()
+    {
+        return $this->render('Panda86MasterBundle:reports:quarterly.html.twig');
+    }
+
+    public function invoicesAction()
+    {
+        return $this->render('Panda86MasterBundle:invoices:index.html.twig');
+    }
+
 }
